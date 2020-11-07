ce43c84b9e3da81ff8088dee1ebdd27019c24bc5
Joni Sutinen <joni.sutinen95@gmail.com>
2020-11-07 20:22:39 +0200

    code cleaning

--- loadimages.py
+++ loadimages.py
@@ -7,6 +7,11 @@ from numpy import savez_compressed
 
 # load all images in a directory into memory
 def load_images(path, size=(256,256)):
+	"""
+    :param path: path to images
+    :param size: size on images, default = 256,256
+    :return: array of data list
+    """
 	data_list = list()
 	# enumerate filenames in directory, assume all are images
 	for filename in listdir(path):
@@ -18,19 +23,23 @@ def load_images(path, size=(256,256)):
 		data_list.append(pixels)
 	return asarray(data_list)
 
-# dataset path
-path = 'city2art/'
-# load dataset A
-dataA1 = load_images(path + 'trainA/')
-dataAB = load_images(path + 'testA/')
-dataA = vstack((dataA1, dataAB))
-print('Loaded dataA: ', dataA.shape)
-# load dataset B
-dataB1 = load_images(path + 'trainB/')
-dataB2 = load_images(path + 'testB/')
-dataB = vstack((dataB1, dataB2))
-print('Loaded dataB: ', dataB.shape)
-# save as compressed numpy array
-filename = 'city2art_256.npz'
-savez_compressed(filename, dataA, dataB)
-print('Saved dataset: ', filename)
+def main():
+	# dataset path
+	path = 'city2art/'
+	# load dataset A
+	dataA1 = load_images(path + 'trainA/')
+	dataAB = load_images(path + 'testA/')
+	dataA = vstack((dataA1, dataAB))
+	print('Loaded dataA: ', dataA.shape)
+	# load dataset B
+	dataB1 = load_images(path + 'trainB/')
+	dataB2 = load_images(path + 'testB/')
+	dataB = vstack((dataB1, dataB2))
+	print('Loaded dataB: ', dataB.shape)
+	# save as compressed numpy array
+	filename = 'city2art_256.npz'
+	savez_compressed(filename, dataA, dataB)
+	print('Saved dataset: ', filename)
+
+if __name__ == "__main__":
+    main()
