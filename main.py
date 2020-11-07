48fee7329f4e24037ff0833f0fa8964f090f2164
Joni Sutinen <joni.sutinen95@gmail.com>
2020-11-07 13:50:57 +0200

    main method

--- main.py
+++ main.py
@@ -262,22 +262,26 @@ def train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoB, c_mode
 			# save the models
 			save_models(i, g_model_AtoB, g_model_BtoA)
 
-# load image data
-dataset = load_real_samples('city2art_256.npz')
-print('Loaded', dataset[0].shape, dataset[1].shape)
-# define input shape based on the loaded dataset
-image_shape = dataset[0].shape[1:]
-# generator: A -> B
-g_model_AtoB = define_generator(image_shape)
-# generator: B -> A
-g_model_BtoA = define_generator(image_shape)
-# discriminator: A -> [real/fake]
-d_model_A = define_discriminator(image_shape)
-# discriminator: B -> [real/fake]
-d_model_B = define_discriminator(image_shape)
-# composite: A -> B -> [real/fake, A]
-c_model_AtoB = define_composite_model(g_model_AtoB, d_model_B, g_model_BtoA, image_shape)
-# composite: B -> A -> [real/fake, B]
-c_model_BtoA = define_composite_model(g_model_BtoA, d_model_A, g_model_AtoB, image_shape)
-# train models
-train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoB, c_model_BtoA, dataset)
+def main():
+	# load image data
+	dataset = load_real_samples('city2art_256.npz')
+	print('Loaded', dataset[0].shape, dataset[1].shape)
+	# define input shape based on the loaded dataset
+	image_shape = dataset[0].shape[1:]
+	# generator: A -> B
+	g_model_AtoB = define_generator(image_shape)
+	# generator: B -> A
+	g_model_BtoA = define_generator(image_shape)
+	# discriminator: A -> [real/fake]
+	d_model_A = define_discriminator(image_shape)
+	# discriminator: B -> [real/fake]
+	d_model_B = define_discriminator(image_shape)
+	# composite: A -> B -> [real/fake, A]
+	c_model_AtoB = define_composite_model(g_model_AtoB, d_model_B, g_model_BtoA, image_shape)
+	# composite: B -> A -> [real/fake, B]
+	c_model_BtoA = define_composite_model(g_model_BtoA, d_model_A, g_model_AtoB, image_shape)
+	# train models
+	train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoB, c_model_BtoA, dataset)
+
+
+main()
