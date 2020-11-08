592834391e62cbf67341942c4fa24240860e2dbd
Joni Sutinen <sutinen.joni@gmail.com>
2020-11-08 10:25:53 +0200

    Update loadimages.py

--- loadimages.py
+++ loadimages.py
@@ -1,9 +1,7 @@
 from os import listdir
-from numpy import asarray, vstack
 from keras.preprocessing.image import img_to_array, load_img
-from numpy import savez_compressed
+from numpy import asarray, savez_compressed, vstack
 
-# load all images in a directory into memory
 def load_images(path, size=(256,256)):
 	"""
     :param path: path to images
@@ -11,32 +9,20 @@ def load_images(path, size=(256,256)):
     :return: array of data list
     """
 	data_list = list()
-	# enumerate filenames in directory, assume all are images
 	for filename in listdir(path):
-		# load and resize the image
-		pixels = load_img(path + filename, target_size=size)
-		# convert to numpy array
-		pixels = img_to_array(pixels)
-		# store
-		data_list.append(pixels)
+		data_list.append(img_to_array(load_img(path + filename, target_size=size)))
 	return asarray(data_list)
 
 def main():
-	# dataset path
+
 	path = 'city2art/'
-	# load dataset A
-	dataA1 = load_images(path + 'trainA/')
-	dataAB = load_images(path + 'testA/')
-	dataA = vstack((dataA1, dataAB))
-	print('Loaded dataA: ', dataA.shape)
-	# load dataset B
-	dataB1 = load_images(path + 'trainB/')
-	dataB2 = load_images(path + 'testB/')
-	dataB = vstack((dataB1, dataB2))
-	print('Loaded dataB: ', dataB.shape)
-	# save as compressed numpy array
+	stackedA = vstack((load_images(path + 'trainA/'), load_images(path + 'testA/')))
+	print('StackedA: ', stackedA.shape)
+	stackedB = vstack((load_images(path + 'trainB/'), load_images(path + 'testB/')))
+	print('StackedB: ', stackedB.shape)
+
 	filename = 'city2art_256.npz'
-	savez_compressed(filename, dataA, dataB)
+	savez_compressed(filename, stackedA, stackedB)
 	print('Saved dataset: ', filename)
 
 if __name__ == "__main__":
